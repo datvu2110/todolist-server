fc28fed8a9f1b2edc2752c845cf24284f7560b84
Derek Vu <beetee@Dereks-MacBook-Pro.local>
2020-11-02 21:54:53 -0600

    Allow CORS

--- src/app.js
+++ src/app.js
@@ -26,13 +26,7 @@ app.use(helmet())
 app.use(cors())
 app.use(bodyParser.json())
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Headers", "Origin,Content-Type, Authorization, x-id, Content-Length, X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    next();
-});
+
 
 app.get('/', (req, res) => {
 	res.send("it is working!!")
